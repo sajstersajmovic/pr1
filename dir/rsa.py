aef06ce849658fd862a8a520805cf96e45ec9357
sajstersajmovic <simon.fojcik07@gmail.com>
2026-01-12 10:17:42 +0100

    Create rsa.py

---
+++ dir/rsa.py
@@ -0,0 +1,12 @@
+#!/bin/python
+from pwn import * p=process("/challenge/run") 
+p.recvuntil(b" (public) n = 0x") 
+n=int(p.recvline().strip.decode(),16)
+p.recvuntil(b" (public) e = 0x") 
+int(p.recvline().strip().decode(),16)
+p.recvuntil(b" (private) d = 0x") 
+int(p.recvline().strip().decode(),16)
+p.recvuntil(b"Flag Ciphertext (hex): ")
+flag = int(p.recvline().strip().decode(),16) 
+pt=pow(flag,d,n)
+print(pt)
